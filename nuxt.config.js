13ab3d300a7fb20d1cdf5fd6a50c8b3f97d465e0
Sebastian hilton <bastianh86@outlook.com>
2023-01-25 19:18:48 -0800

    added security features, new files for commerce, blog, and other future implementations

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,7 @@ export default defineNuxtConfig({
       'nuxt-monaco-editor',
       '@nuxt/content',
       '@nuxtjs/i18n',
+      "nuxt-security",
     ],
 
   /*
--- packages/core/formkit.config.ts
+++
@@ -1,9 +0,0 @@
-import { createProPlugin, repeater, toggle } from '@formkit/pro'
-import defaultConfig from '@formkit/nuxt'
-import '@formkit/themes/genesis'
-import '@formkit/pro/genesis'
-
-const proPlugin = createProPlugin('fk-00000000000', {
-    toggle,
-    repeater
-  })
